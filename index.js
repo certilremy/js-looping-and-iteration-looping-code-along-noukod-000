aad3a5801129fb0d7bd949438d2baeac6c1adfc0
Certil Remy <certil@noukod.com>
2019-02-06 17:01:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ EmployeeNames = ['roger', 'tita', 'billy'];
 function printBadges(EmployeeNames)
 {
 
-  for (let i = 0; i < EmployeeNames.length; i++) {
+  for (let i = 0; i < EmployeeNames.length; i++) 
+  {
           console.log(`Wrapped ${EmployeeNames[i]} and added a bow!`);
       }
 }
+printBadges(EmployeeNames)
 
 function printBadges(){
 
